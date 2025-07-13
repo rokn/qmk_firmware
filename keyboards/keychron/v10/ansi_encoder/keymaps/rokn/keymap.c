19ebf9a502c72fb3c492ccfde39a55eb31534f73
Antonio Mindov <antonio.mindov@redis.com>
2025-07-13 08:47:56 +0300

    Add window navigation

--- keyboards/keychron/v10/ansi_encoder/keymaps/rokn/keymap.c
+++ keyboards/keychron/v10/ansi_encoder/keymaps/rokn/keymap.c
@@ -51,11 +51,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ansi_89(
-        KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,        KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,      KC_F11,      KC_F12,   LCTL(LGUI(KC_Q)),             LGUI(KC_TAB),
-        KC_Y,     KC_GRV,   KC_1,     KC_2,     KC_3,         KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,        KC_0,        KC_MINS,     KC_EQL,   KC_BSPC,             DT_UP,
-        _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,         KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,        KC_LBRC,     KC_RBRC,  KC_BSLS,             DT_DOWN,
-        _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,         KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,     KC_QUOT,               KC_ENT,              DT_PRNT,
-        _______,  KC_LSFT,  KC_Z,     KC_X,     KC_C,         KC_V,     KC_B,                KC_B,     KC_N,     KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,  SC_SENT,   KC_UP,
+        KC_MUTE,  KC_ESC,                KC_F1,    KC_F2,    KC_F3,        KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,      KC_F11,      KC_F12,   LCTL(LGUI(KC_Q)),             LGUI(KC_TAB),
+        KC_Y,     KC_GRV,                KC_1,     KC_2,     KC_3,         KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,        KC_0,        KC_MINS,     KC_EQL,   KC_BSPC,             DT_UP,
+        _______,  KC_TAB,                KC_Q,     KC_W,     KC_E,         KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,        KC_LBRC,     KC_RBRC,  KC_BSLS,             DT_DOWN,
+        _______,  LGUI(LCTL(KC_SPACE)),  KC_A,     KC_S,     KC_D,         KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,     KC_QUOT,               KC_ENT,              DT_PRNT,
+        _______,  KC_LSFT,  KC_Z,        KC_X,     KC_C,         KC_V,     KC_B,                KC_B,     KC_N,     KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,  SC_SENT,   KC_UP,
         _______,  KC_LCTL,  KC_LOPT,            MO(NUMPAD_LAYER),  LT(NAV_LAYER, KC_SPC),   KC_LCMD,                       LT(LAYER_3, KC_SPC),                KC_RCMD,                            KC_LEFT,   KC_DOWN,  KC_RGHT),
 
     [NAV_LAYER] = LAYOUT_ansi_89(
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
         _______,  KC_3,     _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,             _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   LGUI(LSFT(LCTL(KC_LEFT))),  LGUI(KC_H),  LGUI(LSFT(KC_ENT)),  LGUI(LSFT(LCTL(KC_RGHT))),  _______,  _______,             _______,            _______,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,
         _______,  _______,  _______,            _______,  LSFT(LGUI(KC_SPC)),  _______,                       _______,            _______,                       _______,  _______,  _______),
 
@@ -96,7 +96,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [NAV_LAYER] = { ENCODER_CCW_CW(KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK) },
     [NUMPAD_LAYER] = { ENCODER_CCW_CW(KC_BRID, KC_BRIU) },
-    [LAYER_3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LAYER_3] = { ENCODER_CCW_CW(QK_RGB_MATRIX_MODE_PREVIOUS, QK_RGB_MATRIX_MODE_NEXT) },
     [LAYER_4] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif // ENCODER_MAP_ENABLE
